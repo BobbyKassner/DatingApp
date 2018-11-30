b29ba64d2863b825485b69b44a905540b435f18d
Bobby Kassner <bobby.kassner@gmail.com>
2018-11-30 13:40:15 +0200

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace Dating.API.Controllers
 
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
